f7e072d2921d6220b9271714ec2be9c5f0f25b67
Gayani91 <dilruksh91@gmail.com>
2015-04-27 12:34:23 +0530

    app code commit

--- HikingPal/app/src/main/java/com/test/gayaninanayakkara/hikingpal/Adams_Peak.java
+++ HikingPal/app/src/main/java/com/test/gayaninanayakkara/hikingpal/Adams_Peak.java
@@ -1,17 +1,33 @@
 package com.test.gayaninanayakkara.hikingpal;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Adams_Peak extends ActionBarActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adams_peak);
+
+        btn=(Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Adams_Peak.this,MiddleActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
